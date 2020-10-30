cf47f0d050c7ad1542dec59dc819a3d25376e2e4
arshvardhan <arsh.vardhan@robosoftin.com>
2020-10-30 11:36:32 +0530

    Updated Scope Functions

--- app/src/main/java/scope/functions/Let.kt
+++ app/src/main/java/scope/functions/Let.kt
@@ -1,7 +1,27 @@
 package scope.functions
 
 /**
- * The Kotlin standard library function let can be used for scoping and null-checks.
+ * To help choose the right scope function, here is the table of key differences between them.
+ * -------------------------------------------------------------------------------------------
+ * Function	    Object reference	Return value	Is extension function
+ * --------    -----------------    ------------    ---------------------
+ * let	        it	                Lambda result	Yes
+ * run	        this	            Lambda result	Yes
+ * run	        -	                Lambda result	No: called without the context object
+ * with	        this	            Lambda result	No: takes the context object as an argument.
+ * apply	    this	            Context object	Yes
+ * also	        it	                Context object	Yes
+ *
+ * Here is a short guide for choosing scope functions depending on the intended purpose:
+ * - Executing a lambda on non-null objects: 'let'
+ * - Introducing an expression as a variable in local scope: 'let'
+ * - Object configuration: 'apply'
+ * - Object configuration and computing the result: 'run'
+ * - Running statements where an expression is required: non-extension 'run'
+ * - Additional effects: 'also'
+ * - Grouping function calls on an object: 'with'
+
+ * The Kotlin standard library function 'let' can be used for scoping and null-checks.
  * When called on an object, let executes the given block of code and returns the result of its last expression.
  * The object is accessible inside the block by the reference it.
  * */
@@ -32,3 +52,17 @@ fun main() {
     printNonNull(null)
     printNonNull("my string")
 }
+
+//data class Person(var name: String, var age: Int, var city: String) {
+//    fun moveTo(newCity: String) { city = newCity }
+//    fun incrementAge() { age++ }
+//}
+//
+//fun main() {
+//    Person("Alice", 20, "Amsterdam").let {
+//        println(it)
+//        it.moveTo("London")
+//        it.incrementAge()
+//        println(it)
+//    }
+//}
